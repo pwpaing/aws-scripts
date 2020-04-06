4d36a8c6537623625c0c8e0a8d3799f5a2fda346
Gabriel Chamon Araujo <gchamon@live.com>
2020-04-06 14:01:50 -0300

    fix hostname unable to resolve after change

--- elastic-beanstalk-set-hostname-within-instance.sh
+++ elastic-beanstalk-set-hostname-within-instance.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # This script sets the hostname on Elastic Beanstalk servers from within the instance with their EB environment name and public IP address
-# It also will restart New Relic monitoring if present
 # Requires "ec2:Describe*" IAM Policy
 
 # Update boto
@@ -8,32 +7,32 @@ pip install --upgrade boto
 
 # Remove any previous scripts
 function cleanup(){
-	if [ -f /home/ec2-user/ebenvironmentname.py ]; then
-		rm -f /home/ec2-user/ebenvironmentname.py
-	fi
-	if [ -f /home/ec2-user/sethostname.sh ]; then
-		rm -f /home/ec2-user/sethostname.sh
-	fi
+  if [ -f /home/ec2-user/ebenvironmentname.py ]; then
+    rm -f /home/ec2-user/ebenvironmentname.py
+  fi
+  if [ -f /home/ec2-user/sethostname.sh ]; then
+    rm -f /home/ec2-user/sethostname.sh
+  fi
 }
 
 cleanup
 
 # Create the Python script to detect EB Environment
 cat > /home/ec2-user/ebenvironmentname.py <<- EOF
-	#!/usr/bin/env python
+#!/usr/bin/env python
 
-	import boto.utils
-	import boto.ec2
+import boto.utils
+import boto.ec2
 
-	iid_doc = boto.utils.get_instance_identity()['document']
-	region = iid_doc['region']
-	instance_id = iid_doc['instanceId']
+iid_doc = boto.utils.get_instance_identity()['document']
+region = iid_doc['region']
+instance_id = iid_doc['instanceId']
 
-	ec2 = boto.ec2.connect_to_region(region)
-	instance = ec2.get_only_instances(instance_ids=[instance_id])[0]
-	env = instance.tags['elasticbeanstalk:environment-name']
+ec2 = boto.ec2.connect_to_region(region)
+instance = ec2.get_only_instances(instance_ids=[instance_id])[0]
+env = instance.tags['elasticbeanstalk:environment-name']
 
-	print(env)
+print(env)
 EOF
 
 chmod +x /home/ec2-user/ebenvironmentname.py
@@ -41,12 +40,16 @@ chmod +x /home/ec2-user/ebenvironmentname.py
 # Set Hostname
 {
   echo '#!/usr/bin/env bash'
-  echo ebenvironmentname=\$\(./ebenvironmentname.py\)
+  echo ebenvironmentname=\$\(/usr/bin/env python /home/ec2-user/ebenvironmentname.py\)
   echo sudo hostname '"$ebenvironmentname"'-"$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)"
-  echo "chkconfig --list newrelic-sysmond &> /dev/null && sudo service newrelic-sysmond restart"
 } >> /home/ec2-user/sethostname.sh
 chmod +x /home/ec2-user/sethostname.sh
 
 cd /home/ec2-user/ && ./sethostname.sh
+{
+  cat /etc/hosts
+  echo 127.0.0.1 $(hostname)
+} > /tmp/hosts
+sort /tmp/hosts | uniq > /etc/hosts
 
 cleanup
